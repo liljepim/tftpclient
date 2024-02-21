c0bf7f389043bafd1d55aa23d105d19cf3d0cb23
liljepim <95076212+liljepim@users.noreply.github.com>
2024-02-21 23:29:20 +0800

    Major Changes

--- TFTPClient.py
+++ TFTPClient.py
@@ -44,7 +44,9 @@ TIMEOUT = 5
 
 # Request Format: opcode - 0 - filename - 0 - modes
 def send_request(socket, opcode, filename, serverAddress, blksize, mode='octet'):
-    request_packet = struct.pack('!H', opcode) + filename.encode('utf-8') + b'\0' + mode.encode('utf-8') + b'\0' + "blksize".encode('utf-8') + b'\0' + str(blksize).encode('utf-8') + b'\0'
+    request_packet = struct.pack('!H', opcode) + filename.encode('utf-8') + b'\0' + mode.encode('utf-8') + b'\0'
+    if blksize != 512:
+        request_packet += "blksize".encode("utf-8") + b'\0' + str(blksize).encode("utf-8") + b'\0'
     print(len(request_packet))
     print(request_packet)
     socket.sendto(request_packet, serverAddress)
@@ -52,78 +54,128 @@ def send_request(socket, opcode, filename, serverAddress, blksize, mode='octet')
 def receive_packet(sock, blksize):
     data, address = sock.recvfrom(blksize + 4)
     opcode = struct.unpack('!H', data[:2])[0]
+    print(opcode)
     print(len(data))
-    return opcode, data[4:], address[1], len(data)
+    if opcode == 3:
+        return opcode, data[4:], address, len(data)
+    return opcode, data[2:]
+
+def send_packet(sock, blksize, data, blknum):
+    data_packet = struct.pack('!H', 3) + struct.pack('!H', blknum) + data
+    
 
 def receive_oack(sock):
-    data, address = sock.recvfrom(4)
-    opcode = struct.unpack('!H', )
+    data, address = sock.recvfrom(516)
+    opcode = struct.unpack('!H', data[:2])[0]
+    if opcode == 6:
+        options = data[2:].decode('utf-8').split('\0')
+        return int(options[1]), address
+    print(data[2:].decode('utf-8'))
+    return None, address
+
+def send_ack(sock, blknum, server_address):
+    ack_packet = struct.pack('!HH', 4, blknum)
+    sock.sendto(ack_packet, server_address)
 
 def send_packet(sock, opcode, blknum, data):
     sock.send(data)
 
+def downloadfile(sock, sa):
+    clientSocket = sock
+    server_address = sa
+    filename = input("Please enter filename of file to download: ")
+    blknum = 1
+    bfile = open(filename, 'wb')
+    try:
+        while True:
+            blksize = int(input("Enter desired block size (Default - 512): "))
+            if blksize >= 8 and blksize <= 65464:
+                break
+            else:
+                print("Invalid Block Size! Input another value between 8 and 65464.")
+        send_request(clientSocket, RRQ, filename, server_address, blksize)
+        if blksize != 512:
+                blksize, server_address = receive_oack(clientSocket)
+                send_ack(clientSocket, 0, server_address)
+        if blksize == None:
+            os.remove(filename)
+
+        while True and blksize != None:
+            # Receive data packet from the server
+            opcode, received_data, server_address, size = receive_packet(clientSocket, blksize)
+            if opcode == 3:  # Data packet
+                # Process the received data (write to file, print, etc.)
+                print(f"Received data block {blknum}")
+                bfile.write(received_data)
+                # ...
+
+                # Send acknowledgment
+                send_ack(clientSocket, blknum, server_address)
+
+                blknum += 1
+
+            elif opcode == 5:  # Error packet
+                error_code = struct.unpack('!H', received_data[:2])[0]
+                error_msg = received_data[2:].decode('utf-8')
+                print(f"Error {error_code}: {error_msg}")
+                bfile.close()
+                os.remove(filename)
+                break
+            elif opcode == 4:
+                print(opcode)
+            else:
+                print(f"Unexpected opcode: {opcode}")
+                break
+
+            if size >= 0 and size < blksize:
+                bfile.close()
+                break
+    except KeyboardInterrupt:
+        print("Client interrupted by user")
+    except Exception as e:
+        print(e)
+
+def uploadfile(sock, sa):
+    clientSocket = sock
+    server_address = sa
+    filename = input("Please enter filename of file to upload: ")
+    try:
+        file = open(filename, 'rb')
+        filedata = file.read()
+        print(type(filedata))
+    except Exception as e:
+        print(e)
+    while True:
+        blksize = int(input("Enter desired block size (Default - 512): "))
+        if blksize >= 8 and blksize <= 65464:
+            break
+        else:
+            print("Invalid Block Size! Input another value between 8 and 65464.")
+    send_request(clientSocket, WRQ, filename, server_address, blksize)
+    if blksize != 512:
+        blksize, server_address = receive_oack(clientSocket)
+        send_ack(clientSocket, 0, server_address)
+        
+    #Split to multiple segments depending on block size
+
+
+
 def tftp_client(clientAddress, port):
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    clientSocket.bind((clientAddress, port))
     server_ip = input("Please enter IP address of server: ")
-    server_address = (server_ip, 69)
     while True:
-        print(f'[1] Download File\n[2] Upload File\n[3] Exit')
-        choice = int(input("Input: "))
-        serverPort = 69
+        server_address = (server_ip, TFTP_PORT)
+        try:
+            print(f'[1] Download File\n[2] Upload File\n[3] Exit')
+            choice = int(input("Input: "))
+        except Exception as e:
+            choice = 4
         match choice:
             case 1:
-                filename = input("Please enter filename of file to download: ")
-                blockNumber = 1
-                bfile = open(filename, 'wb')
-                try:
-                    blksize = int(input("Enter desired block size (Default - 512): "))
-                    send_request(clientSocket, RRQ, filename, server_address, blksize)
-                    while True:
-                        # Receive data packet from the server
-                        opcode, received_data, serverPort, size = receive_packet(clientSocket, blksize)
-                        if opcode == 3:  # Data packet
-                            # Process the received data (write to file, print, etc.)
-                            print(f"Received data block {blockNumber}")
-                            print(received_data)
-                            bfile.write(received_data)
-                            # ...
-
-                            # Send acknowledgment
-                            server_address[1] = serverPort
-                            ack_packet = struct.pack('!HH', 4, blockNumber)
-                            clientSocket.sendto(ack_packet, server_address)
-
-                            blockNumber += 1
-
-                        elif opcode == 5:  # Error packet
-                            error_code = struct.unpack('!H', received_data[:2])[0]
-                            error_msg = received_data[2:].decode('utf-8')
-                            print(f"Error {error_code}: {error_msg}")
-                            bfile.close()
-                            os.remove(filename)
-                            break
-                        elif opcode == 4:
-                            print(opcode)
-                        else:
-                            print(f"Unexpected opcode: {opcode}")
-                            break
-
-                        if size >= 0 and size < 516:
-                            bfile.close()
-                            break
-                except KeyboardInterrupt:
-                    print("Client interrupted by user")
+                downloadfile(clientSocket, server_address)
 
             case 2:
-                filename = input("Please enter filename to upload: ")
-                blockNumber = 1
-                try:
-                    bfile = open(filename, 'rb')
-                    send_request(clientSocket, WRQ, filename, server_address)
-
-                except Exception as e:
-                    print(e)
+                uploadfile(clientSocket, server_address)
 
             case 3:
                 print("Closing client application")
